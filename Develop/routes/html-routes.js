24f4b69b75eb82afbc8b461383ca856e86139a2b
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-07-01 20:59:05 -0400

    adding api routes to routes.js

--- Develop/routes/html-routes.js
+++ Develop/routes/html-routes.js
@@ -1,6 +1,24 @@
+const fs = require('fs');
 const path = require('path'); 
 
 module.exports = function (app) {
+
+    fstat.readFile('db/db.json','utf8', (err,data) => {
+        if (err) throw err;
+
+        let notes = JSON.parse(data);
+
+        // API Routes
+        app.get('/api/notes', (req, res) => res.json(notes));
+        
+    })
+
+        app.post('api/notes', (req, res) => {
+            let newNotes = req.body;
+            notes.push(newNotes);
+            updateDb();
+            return console.log(`Added Note: ${newNotes.title}`);
+        })
     
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/../public/notes.html')));
 
