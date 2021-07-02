bf7b2105e5f0f5f872b43619ff0b175b66a6307e
mcnultyshane <82954885+mcnultyshane@users.noreply.github.com>
2021-07-01 20:15:35 -0400

    troubleshooting server routes.

--- Develop/server.js
+++ Develop/server.js
@@ -8,11 +8,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true}));
 // app.use(expect.json());
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
-
-
+require('./routes/html-routes.js')(app)
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT; ${PORT}`);
